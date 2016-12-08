fe9a0788d9635df589a8bda206cf806b9bd9b4eb
Serhii Zavodko <szavodko@SZAVODKO-PC>
2016-12-08 16:51:07 +0200

    Done.

--- filesRenamer/filesRenamer/filesRenamer.py
+++ filesRenamer/filesRenamer/filesRenamer.py
@@ -1,4 +1,5 @@
 import os
+os.chdir("C:\\Users\\szavodko.DEV\\Desktop\\renamer\\")
 for filename in os.listdir("."):
     if not filename.endswith(".py"):
         os.rename(filename,"renamed_"+filename)
